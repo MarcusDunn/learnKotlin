a0ed76bb066e67b8bb1f937855639b49cfaa9415
MarcusDunn <marcus.s.dunn@gmail.com>
2021-11-18 20:22:46 -0800

    added comments

--- src/main/kotlin/Iterators.kt
+++ src/main/kotlin/Iterators.kt
@@ -8,16 +8,25 @@ fun main() {
 // this is more advanced, but idiomatic kotlin that does the same thing
 fun findTop10V2() {
     val top10 = theKingJamesBible
-        .readText()
-        .split(' ')
-        .fold(HashMap<String, Int>()) { acc, s ->
-            acc[s] = (acc[s] ?: 0) + 1
-            acc
+        // to avoid having the full file in memory, we read line by line
+        .readLines()
+        // turn the line into words, use flatMap instead of map, so we still can pretend it's a massive list of words
+        .flatMap { it.split(' ') }
+        // We create a hashmap and populate it word by word.
+        .fold(HashMap<String, Int>()) { hashmapAccumulator, word ->
+            // if you figure out what this does, you've got nullability down!
+            hashmapAccumulator[word] = (hashmapAccumulator[word] ?: 0) + 1
+            // return the updated hashmap to the next iteration of fold
+            hashmapAccumulator
         }
+        // turn into a list as hashmaps are unordered so we can't sort
         .toList()
-        .sortedByDescending { it.second }
+        // sort by the number of occurrences descending.
+        .sortedByDescending { (_, occurrences) -> occurrences }
+        // take the top 10
         .take(10)
-        .map { it.first }
+        // remove the number of occurrences, so we only have the words
+        .map { (word, _) -> word }
     println(top10)
 }
 
